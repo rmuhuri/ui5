77a770411c3ade85bdb173fc856091d29f7435f3
rmuhuri <rmuhuri@gmail.com>
2021-09-29 01:23:29 -0500

    7

---
+++ 7_json_models/webapp/controller/App.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"], function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("x.controller.App", {
+        onInit: function () {
+            var oDate = {
+                recipient: {
+                    name: "world"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+        onShowHello: function () {
+            MessageToast.show("Hello World");
+        }
+    });
+});
